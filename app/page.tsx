2510b97a901297737bb97f3b508a6b68f0bac1ce
Yor <yorjungai@gmail.com>
2026-02-20 18:47:30 +0700

    fix: add use client to page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import dynamic from "next/dynamic";
 const SummerQuiz = dynamic(() => import("../components/SummerQuiz"), { ssr: false });
 export default function Home() { return <SummerQuiz />; }
